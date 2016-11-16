d0ba3bc65a2d31b466b6c1cb44b3bcd907807e35
Khai Pham <khaicpham@gmail.com>
2016-11-15 18:16:43 -0800

    Home screen prototype under /dev

---
+++ dev/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Coffee Guide</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="background-color:#EDEDED">
+  <div class="categories-container">
+    <div class="categories-title">American Classics</div>
+    <div class="categories-subtitle">Some of our personal favorites</div>
+    <div class="categories-drinks">
+      <div class="drink-container" style="background-color:#AD8C6F">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 1.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#FDCCAA">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 2.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#F69D9D">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 3.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#AEBDE1">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 4.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#B7DBA5">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 5.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#F4F2AD">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 6.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+    </div>
+  </div>
+  <div class="categories-container">
+    <div class="categories-title">Dark Burst</div>
+    <div class="categories-subtitle">Our strongest recommendations for strongest drinkers</div>
+    <div class="categories-drinks">
+      <div class="drink-container" style="background-color:#FDCCAA">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 2.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#B7DBA5">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 5.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#F4F2AD">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 6.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+      <div class="drink-container" style="background-color:#F69D9D">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 3.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+        <div class="drink-container" style="background-color:#AD8C6F">
+          <div class="drink-img">
+            <img src="images/coffeesvgs/Artboard 1.svg" alt="" />
+          </div>
+          <div class="drink-name">Machiato</div>
+        </div>
+      <div class="drink-container" style="background-color:#AEBDE1">
+        <div class="drink-img">
+          <img src="images/coffeesvgs/Artboard 4.svg" alt="" />
+        </div>
+        <div class="drink-name">Machiato</div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
