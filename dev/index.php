016f181c57e6b888f83ed34e37884c6f413e716b
Khai Pham <khaicpham@gmail.com>
2016-11-30 01:28:14 -0800

    JSONifying

--- dev/index.php
+++ dev/index.php
@@ -4,10 +4,14 @@ $filename = "data/drinks.json";
 $file = file_get_contents($filename);
 $data = json_decode($file);
 
+$categoriesFilename = "data/categories.json";
+$categoriesFile = file_get_contents($categoriesFilename);
+$categoriesData = json_decode($categoriesFile);
+
 $americanClassics = array_filter($data, function($obj){return $obj->drinkCategory == "American Classics";});
 $darkBurst = array_filter($data, function($obj){return $obj->drinkCategory == "Dark Burst";});
 
-$darkBurstEncoded = json_encode($darkBurst);
+$categories = [$americanClassics,$darkBurst];
 
 function showDrinks($id,$category){
   return "
@@ -17,8 +21,7 @@ function showDrinks($id,$category){
       </div>
       <div class='drink-name'>{$category[$id]->drinkName}</div>
     </a>";
-    }
-
+}
 function showCategories($category) {
   $total = count($category);
   for($i=0; $i<$total; $i++) {
@@ -26,7 +29,39 @@ function showCategories($category) {
     else {echo showDrinks($i,$category);}
   }
 }
-
+function showDrinkDetail($drink) {
+  $currentID=$_GET['id'];
+  ?>
+    <body style="background-color:rgb(20,20,20)">
+      <div class="card-container" style="background-color:<?php echo $drink->drinkBgrColor?>">
+        <div class="card-name"><?php echo $drink->drinkName?></div>
+        <div class="ingredients-container">
+          <ul class="ingredients-list">
+            <?php
+            $ingredients = $drink->ingredients;
+              for ($i=0; $i<count($ingredients); $i++) {
+                echo "<li>".$ingredients[$i]->qty." <span class='uppercase'>".$ingredients[$i]->ingredient."</span></li>";
+              }
+            ?>
+          </ul>
+        </div>
+        <div class="steps-container">
+          <ul class="steps-list">
+            <?php
+            $steps = $drink->steps;
+              for ($i=0; $i<count($steps); $i++) {
+                echo "<li>".$steps[$i]->step."</li>";
+              }
+            ?>
+          </ul>
+        </div>
+        <div class="card-img">
+          <img src="images/coffeesvgs/<?php echo $drink->drinkImg ?>" alt="" />
+        </div>
+      </div>
+    </body>
+  <?php
+  }
 ?>
 
 <!DOCTYPE html>
@@ -39,20 +74,25 @@ function showCategories($category) {
   <meta name="mobile-web-app-capable" content="yes">
   <link rel="stylesheet" href="css/style.css">
 </head>
-<body style="background-color:#EDEDED">
-  <div class="categories-container">
-    <div class="categories-title">American Classics</div>
-    <div class="categories-subtitle">Some of our personal favorites</div>
-    <div class="categories-drinks">
-      <?php echo showCategories($americanClassics);?>
-    </div>
-  </div>
-  <div class="categories-container">
-    <div class="categories-title">Dark Burst</div>
-    <div class="categories-subtitle">Our strongest recommendations for strongest drinkers</div>
-    <div class="categories-drinks">
-      <?php echo showCategories($darkBurst);?>
-    </div>
-  </div>
-</body>
+<?php
+  if(isset($_GET['id'])) {
+    showDrinkDetail($data[$_GET['id']]);
+  } else {
+    global $categories;
+    global $data;
+    echo "<body style='background-color:#EDEDED'>";
+    for($i=0; $i<count($categoriesData); $i++){
+    ?>
+      <div class="categories-container">
+        <div class="categories-title"><?php echo $categoriesData[$i]->categoryName ?></div>
+        <div class="categories-subtitle"><?php echo $categoriesData[$i]->categoryDes ?></div>
+        <div class="categories-drinks">
+          <?php echo showCategories($categories[$i]);?>
+        </div>
+      </div>
+    <?php
+    echo "</body>";
+    }
+  }
+?>
 </html>
